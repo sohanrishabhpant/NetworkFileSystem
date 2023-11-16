6e5d6b4c6277fdfe22c2f199c9844e840045358d
jahnu05 <127508847+jahnu05@users.noreply.github.com>
2023-11-17 02:22:52 +0530

    3

--- nm.c
+++ nm.c
@@ -3,13 +3,28 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-typedef struct details{
-    char ip[100];
-    int port_no;
-    char list_of_accesible_paths[200];
-    int client_port_no;
-}details;
+#include <pthread.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+int server_sock;
+socklen_t addr_size;
+#define BUFFER_SIZE 1000
+typedef char* string;
+typedef struct details
+{
+  char ip[100];
+  int port_no;
+  string list_of_accesible_paths[200];
+  int client_port_no;
+} details;
+typedef struct tokenised{
+  string* buffer;
+}tokenised;
 details ss_dets[100];
+int ss_count=0;
+int client_sock[1000];
+struct sockaddr_in client_addr[1000];
 char **gettokens(char *token)
 {
   int count;
@@ -30,75 +45,191 @@ char **gettokens(char *token)
   argv[count] = NULL;
   return argv;
 }
-int main() {
+char *ip = "127.0.0.1";
+int port = 5000;
 
-  char *ip = "127.0.0.1";
-  int port = 5000;
 
-  int server_sock, ss_sock;
-  struct sockaddr_in server_addr, ss_addr;
-  socklen_t addr_size;
+void handle_client(char* buffer,int count){
+  char **tokens=malloc(sizeof(char*)*1000);
+  for (int i=0;i<1000;i++){
+    tokens[i]=malloc(sizeof(char)*1000);
+  }
+  tokens=gettokens(buffer);
+  string req_path=tokens[2];
+  int i;
+  int index;
+  for ( i = 0; i < ss_count; i++)
+  {
+    index=-1;
+    int j=0;
+    while (1)
+    {
+      if (strcmp(ss_dets[i].list_of_accesible_paths[j],req_path)==0)
+      {
+        index=j;
+        break;
+      }
+      j++;
+    }
+  }
+  if (strcmp(tokens[0],"READ")==0||strcmp(tokens[0],"WRITE")==0||strcmp(tokens[0],"GET_DETAILS")==0)
+  {
+    int ss_port=ss_dets[index].port_no;
+    char* ip_add=ss_dets[index].ip;
+    client_sock[count] = accept(server_sock, (struct sockaddr *)&client_addr[count], &addr_size);
+    int n = send(client_sock[count], ip_add, strlen(ip_add), 0);
+    if (n<0)
+    {
+      perror("send");
+    }
+    n = send(client_sock[count], &ss_port, strlen(ss_port), 0);
+    if (n<0)
+    {
+      perror("send");
+    }
+    //wait for ack from ss.
+  }
+  else{
+  int ss_port=ss_dets[index].port_no;
+  int new_sock = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in new_addr;
+  if (new_sock < 0)
+  {
+    perror("[-]Socket error");
+        exit(1);
+  }
+  printf("[+]TCP server socket created.\n");
+
+  // Error handling for bind() call
+  memset(&new_addr, '\0', sizeof(new_addr));
+  new_addr.sin_family = AF_INET;
+  new_addr.sin_port = htons(ss_port);
+  new_addr.sin_addr.s_addr = inet_addr(ip);
+  int n = connect(new_sock, (struct sockaddr*)&new_addr, sizeof(new_addr));
+  if (n < 0) {
+    perror("Connect error");
+    exit(1);
+  }
+  printf("Connected to the naming server.\n");
+tokenised token;
+token.buffer=buffer;
+if( send(new_sock,(void*)&token, sizeof(token),0) < 0 ) {
+    printf("send failed!\n");
+}
+    perror("Send error");
+  // close(sock);
+  }
+ 
+}
+
+void *receive_buffer(void *arg) {
+  // Code to receive buffer from client
+  char buffer[1000];
+  // Error handling for listen() call
+  listen(server_sock, 1000);
+  int count=0;
+  while (1)
+  {
+    addr_size = sizeof(client_addr[count]);
+    client_sock[count] = accept(server_sock, (struct sockaddr *)&client_addr[count], &addr_size);
+    // Error handling for recv() call
+    bzero(buffer, 1024);
+    recv(client_sock[ss_count],buffer, sizeof(buffer), 0);
+    count++;
+    handle_client(buffer,count);
+  }
+}
+void *accept_ss(void *arg) {
+
+}
+
+
+int main()
+{
+  // pthread_t thread1, thread2;
+  // int ret1, ret2;
+  // ret1 = pthread_create(&thread1, NULL, &receive_buffer, NULL);
+  // ret2 = pthread_create(&thread2, NULL, &accept_ss, NULL);
+  // if (ret1)
+  // {
+  //   printf("Error: pthread_create() failed\n");
+  //   exit(EXIT_FAILURE);
+  // }
+  // if (ret2)
+  // {
+  //   printf("Error: pthread_create() failed\n");
+  //   exit(EXIT_FAILURE);
+  // }
+  int  ss_sock[1000];
+  struct sockaddr_in server_addr, ss_addr[1000];
   char buffer[1024];
   int n;
 
   // Error handling for socket() call
   server_sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_sock < 0) {
+  if (server_sock < 0)
+  {
     perror("[-]Socket error");
-    exit(1);
+        exit(1);
   }
   printf("[+]TCP server socket created.\n");
 
   // Error handling for bind() call
   memset(&server_addr, '\0', sizeof(server_addr));
   server_addr.sin_family = AF_INET;
-  server_addr.sin_port = port;
+  server_addr.sin_port = htons(port);
   server_addr.sin_addr.s_addr = inet_addr(ip);
 
-  n = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if (n < 0) {
+  n = bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (n < 0)
+  {
     perror("Bind error");
     exit(1);
   }
   printf("Bind to the port number: %d\n", port);
 
   // Error handling for listen() call
-  listen(server_sock, 5);
+  listen(server_sock, 1000);
   details nm_details;
   memset(&nm_details, '\0', sizeof(struct details));
-    
-    int ss_count=0;
-  while (1) {
-    addr_size = sizeof(ss_addr);
-    ss_sock = accept(server_sock, (struct sockaddr*)&ss_addr, &addr_size);
-    printf("ss %d connected.\n",ss_count);
-    memset(&ss_dets[ss_count],'\0',sizeof(struct details));
-    
+
+  int ss_count = 0;
+  while (1)
+  {
+    addr_size = sizeof(ss_addr[ss_count]);
+    ss_sock[ss_count] = accept(server_sock, (struct sockaddr *)&ss_addr[ss_count], &addr_size);
+    printf("ss %d connected.\n", ss_count);
+    memset(&ss_dets[ss_count], '\0', sizeof(struct details));
+
     // Error handling for recv() call
     bzero(buffer, 1024);
-    recv(ss_sock, &ss_dets[ss_count], sizeof(details), 0);
-    printf("ss %d ip: %s\n",ss_count,ss_dets[ss_count].ip);
-    printf("ss %d port_no: %d\n",ss_count,ss_dets[ss_count].port_no);
-    printf("ss %d list_of_paths: %s\n",ss_count,ss_dets[ss_count].list_of_accesible_paths);
-    printf("ss %d client_port_no: %d\n",ss_count,ss_dets[ss_count].client_port_no);
-    // strcpy(buffer,"CREATE file test/");
-    fgets(buffer,1000,stdin);
-    char **argv2 = malloc(sizeof(char *) * 1000);
-  for (int i = 0; i < 1000; i++)
-  {
-    argv2[i] = malloc(sizeof(char) * 1000);
-  }
-  argv2=gettokens(buffer);
-    n = send(ss_sock, argv2, strlen(buffer), 0);
-    if (n < 0) {
-        perror("Send error");
-        exit(1);
+    recv(ss_sock[ss_count], &ss_dets[ss_count], sizeof(details), 0);
+    printf("ss %d ip: %s\n", ss_count, ss_dets[ss_count].ip);
+    printf("ss %d port_no: %d\n", ss_count, ss_dets[ss_count].port_no);
+    int i=0;
+    while (ss_dets[ss_count].list_of_accesible_paths[i]!=NULL)
+    {
+      nm_details.list_of_accesible_paths[i] = malloc(sizeof(char)*1000);
+      printf("ss %d list_of_paths: %s\n", ss_count, ss_dets[ss_count].list_of_accesible_paths[i]);
+      i++;
+    }
+    
+    printf("ss %d client_port_no: %d\n", ss_count, ss_dets[ss_count].client_port_no);
+    n = send(ss_sock[ss_count], buffer, strlen(buffer), 0);
+    if (n < 0)
+    {
+      perror("Send error");
+      exit(1);
     }
     printf("data sent\n");
     ss_count++;
+    if (getchar()=='\n'){
+      break;
+    }
   }
-    close(ss_sock);
-    printf("[+]Client disconnected.\n\n");
-    
+  
+  // close(ss_sock);
+  printf("[+]Client disconnected.\n\n");
+
   return 0;
 }
--- ss.c
+++ ss.c
@@ -3,44 +3,99 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
+typedef char *string;
 
 typedef struct details
 {
   char ip[100];
   int port_no;
-  char list_of_accesible_paths[200];
+  string list_of_accesible_paths[200];
   int client_port_no;
 } details;
-
-void create_func(char **argv2,details nm_details){
- if(strcmp(argv2[3],nm_details.list_of_accesible_paths)==0){
-
-      printf("helloc\n");
-      if (strcmp(argv2[1],"-d")==0){
-        char *kris;
-        kris=malloc(sizeof(char)*1000);
-        snprintf(kris, 1000, "%s/%s", argv2[3], argv2[2]);
-        int stat=mkdir(kris,S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-        if(stat==0){
-          printf("mkdir is succesfully updated");
-        }
-      }
-      if(strcmp(argv2[1],"-f")==0){
-        char *kris;
-        kris=malloc(sizeof(char)*1000);
-        snprintf(kris, 1000, "%s/%s", argv2[3], argv2[2]);
-        int ris=open(kris,O_RDWR | O_CREAT ,S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-        if(ris!=-1){
-          printf("file created succesfully\n");
-        }
-
-      }
+string *paths;
+typedef struct tokenised
+{
+  string* buffer;
+} tokenised;
+void create_func(char **argv2)
+{
+  printf("helloc\n");
+  if (strcmp(argv2[1], "-d") == 0)
+  {
+    char *kris;
+    kris = malloc(sizeof(char) * 1000);
+    snprintf(kris, 1000, "%s/%s", argv2[3], argv2[2]);
+    int stat = mkdir(kris, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    if (stat == 0)
+    {
+      printf("mkdir is succesfully updated");
     }
+  }
+  if (strcmp(argv2[1], "-f") == 0)
+  {
+    char *kris;
+    kris = malloc(sizeof(char) * 1000);
+    snprintf(kris, 1000, "%s/%s", argv2[3], argv2[2]);
+    int ris = open(kris, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if (ris != -1)
+    {
+      printf("file created succesfully\n");
+    }
+  }
 }
-void connect_ss(char **argv, int port)
+void delete_func(char **argv2)
+{
+  char *kris;
+  kris = malloc(sizeof(char) * 1000);
+  snprintf(kris, 1000, "%s/%s", argv2[3], argv2[2]);
+  if (remove(kris) == 0)
+  {
+    printf("is deleted\n");
+  }
+  else
+  {
+    perror("there is an error\n");
+    exit(1);
+  }
+}
+
+void init_paths(string **paths)
+{
+  *paths = (string *)malloc(sizeof(string) * 200);
+  for (int i = 0; i < 200; i++)
+  {
+    (*paths)[i] = NULL;
+    (*paths)[i] = (string)malloc(sizeof(char) * 1000);
+  }
+}
+void listFiles(const char *path)
+{
+
+  DIR *dir;
+  struct dirent *entry;
+
+  if ((dir = opendir(path)) == NULL)
+  {
+    perror("opendir");
+    exit(EXIT_FAILURE);
+  }
+  int i = 0;
+  while ((entry = readdir(dir)) != NULL)
+  {
+    // printf("%s\n", entry->d_name);
+    strcpy(paths[i], entry->d_name);
+    paths[i][strlen(paths[i]) - 1] = '\0';
+    i++;
+  }
+  closedir(dir);
+}
+
+void connect_ss(int port1, int port)
 {
   int sock;
   char buffer[1024];
@@ -48,10 +103,29 @@ void connect_ss(char **argv, int port)
   socklen_t addr_size;
   details nm_details;
   memset(&nm_details, '\0', sizeof(struct details));
-  strcpy(nm_details.ip, "10.2.131.73");
+  strcpy(nm_details.ip, "127.0.0.1");
   nm_details.client_port_no = 2000;
-  strcpy(nm_details.list_of_accesible_paths, "./rishabh");
-  nm_details.port_no = atoi(argv[1]);
+  char dir_path[FILENAME_MAX];
+  if (fgets(dir_path, sizeof(dir_path), stdin) != NULL)
+  {
+    if (dir_path[strlen(dir_path) - 1] == '\n')
+    {
+      dir_path[strlen(dir_path) - 1] = '\0';
+    }
+  }
+  init_paths(&paths);
+  // paths[0]="sohan";
+  // printf("%s\n",paths[0]);
+  listFiles(dir_path);
+  int i = 0;
+
+  while ((paths)[i] != NULL)
+  {
+    nm_details.list_of_accesible_paths[i] = malloc(strlen((paths)[i]) + 1);
+    strcpy(nm_details.list_of_accesible_paths[i], (paths)[i]);
+    i++;
+  }
+  nm_details.port_no = port1;
   int n;
 
   // Error handling for socket() call
@@ -66,7 +140,7 @@ void connect_ss(char **argv, int port)
   // Error handling for connect() call
   memset(&addr, '\0', sizeof(addr));
   addr.sin_family = AF_INET;
-  addr.sin_port = port;
+  addr.sin_port = htons(port);
   addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
   n = connect(sock, (struct sockaddr *)&addr, sizeof(addr));
@@ -84,29 +158,53 @@ void connect_ss(char **argv, int port)
   perror("Send error");
   // close(sock);
   bzero(buffer, 1024);
-  // n = recv(sock, buffer, sizeof(buffer), 0);
-  if (n < 0)
-  {
-    perror("Recv error");
+}
+void reciving(int port1){
+  char *ip="127.0.0.1";
+  int socket1=socket(AF_INET,SOCK_STREAM,0);
+  if(socket1==-1){
+    perror("socket creation");
     exit(1);
   }
-  char **argv2 = malloc(sizeof(char *) * 1000);
-  for (int i = 0; i < 1000; i++)
-  {
-    argv2[i] = malloc(sizeof(char) * 1000);
+  struct sockaddr_in server;
+  server.sin_family=AF_INET;
+  server.sin_port=htons(port1);
+  server.sin_addr.s_addr=inet_addr(ip);
+  int binding=bind(socket1,(struct sockaddr*)&server,sizeof (server));
+  if (binding<0){
+    perror("bind problem\n");
+    exit(3);
   }
-  // argv2 = gettokens(buffer);
-  n = recv(sock, argv2,, 0);
-  if (strcmp(argv2[0],"create")==0){
-    printf("hello\n");
-    argv2[3][strlen(argv2[3])-1]='\0';
-    // printf("%s %s",argv2[3],nm_details.list_of_accesible_paths);
-    create_func(argv2,nm_details);
-  }
+  int listening=listen(socket1,1000);
+  struct sockaddr_in nm;
+  tokenised token;
+    int accepting=accept(socket1,(struct sockaddr*)&nm,sizeof(nm));
+    int reciving=recv(accepting,&token,sizeof(token),0);
+    if (strcmp(token.buffer[0],"CREATE")==0){
+      create_func(token.buffer);
+    }
+    if (strcmp(token.buffer[0],"DELETE")==0){
+      delete_func(token.buffer);
+    }
+    if (strcmp(token.buffer[0],"COPY")==0){
+      copy_func(token.buffer);
+    }
+    if (strcmp(token.buffer[0],"READ")==0){
+      read_func(token.buffer);
+    }
+    if (strcmp(token.buffer[0],"WRITE")==0){
+      write_func(token.buffer);
+    }
+    if (strcmp(token.buffer[0],"GET_DETAILS")==0){
+      get_details_func(token.buffer);
+    }
+  
 }
 int main(int argc, char **argv)
 {
   int port = 5000;
-  connect_ss(argv, port);
+  connect_ss(atoi(argv[2]), port);
+  reciving(atoi(argv[2]));
+  free(paths);
   return 0;
 }
